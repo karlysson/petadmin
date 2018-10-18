9d6de3b5156ae4b8e13d72b88324d58ce90d5576
Karlysson Alves <karlysson.alves@gmail.com>
2018-10-18 16:03:56 -0300

    index

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,17 @@
 class Client < ApplicationRecord
   include Fae::BaseModelConcern
+  validates :phone, presence: true
+  validates :email,
+    uniqueness: true,
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'Formato do email é invalido e único.'
+    }
+
   def fae_display_field
     name
   end
 
 end
+
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,14 @@
 class Supplier < ApplicationRecord
   include Fae::BaseModelConcern
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :email,
+    uniqueness: true,
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'Formato do email é invalido e único.'
+    }
   def fae_display_field
     name
   end
